a2860478b8ee79911947992bfcb017a68db27f7d
molguin <molguin@activapyme.mx>
2020-04-14 12:09:39 -0700

    Clases agregadas DemoHashMap y DemoArreglos

---
+++ DemoArreglos.java
@@ -0,0 +1,22 @@
+public class DemoArreglos
+{
+    
+    public DemoArreglos(){
+    }
+    
+    public static void main() {
+        String [] arrCadenas = new String[10];
+        arrCadenas[3] = "cadena3";        
+        //System.out.println(arrCadenas.length);
+        
+        Integer[][] matrizEnteros = new Integer[10][5];
+        matrizEnteros[0][0] = 5;
+        matrizEnteros[0][1] = 7;
+        matrizEnteros[1][0] = 3;
+        
+        System.out.println(matrizEnteros[1][0]);
+        
+    }
+
+   
+}
--- DemoHashMap.java
+++ DemoHashMap.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.ArrayList;
+
 public class DemoHashMap
 {
     
@@ -6,7 +8,27 @@ public class DemoHashMap
     }
     
     public static void main() {
+        HashMap<Barco,Avion> hm = new HashMap<>();
+        Barco b = new Barco("M1","rojo");
+        Barco b2 = new Barco("M2","azul");
         
+        hm.put(b,new Avion("MA1","plata"));
+        hm.put(b2,new Avion("MA2","verde"));
+        System.out.println(hm.get(b2).getMarca());
+        
+        hm.forEach((barco,avion) ->{
+            System.out.println(barco.getMarca() + ":" +avion.getColor());
+        });
+        
+        ArrayList<String> a = new ArrayList<>();
+        a.add("cadena1");
+        a.add("cadena2");
+        a.add("cadena3");
+        a.add("cadena4");
+        
+        a.forEach( c -> {
+            System.out.println(c);
+        });
     }
 
    
